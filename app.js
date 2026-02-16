0aa8903715d0e1f6ddd8d82e16e6d547b7b5dd0f
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2026-02-16 20:33:49 +0500

    js-assignment calculator

--- app.js
+++ app.js
@@ -2,63 +2,58 @@
 // --------------------------Asssignment Calculator ---------------------------- //
 
 var display = document.getElementById("calculation");
-var currentInput = "";
+var num1 = "";
+var num2 = "";
+var operator = "";
 
 function inputNumber(num) {
-    currentInput += num;
-    display.value = currentInput;
+    display.value += num;
+}
+function setOperator(op) {
+    if (display.value == "") return;
+
+    num1 = display.value;
+    operator = op;
+    display.value = num1 + " " + operator + " ";
 }
 
 function calculate() {
-    var word = document.getElementById("calculation").value;
-    var operator = "";
-    for (var i = 0; i < word.length; i++) {
-        if (word[i] === "*" || word[i] === "+" || word[i] === "-" || word[i] === "/" || word[i] === "%") {
-            operator = word[i];
-            break;
-        }
-    }
-
-    var index = word.indexOf(operator);
-    var num3 = word.slice(0, index);
-    var num4 = word.slice(index + 1);
-    var num1 = Number(num3);
-    var num2 = Number(num4);
+    var word = document.getElementById("calculation").value.split(" ");
+    num1 = Number(word[0]);
+    operator = word[1];
+    num2 = Number(word[2]);
     document.getElementById("calculation").value = "";
     var result;
 
-    if (operator === "+") {
+    if (operator == "+") {
         result = num1 + num2;
-    } else if (operator === "-") {
-        result = num1 - num2;
-    } else if (operator === "/") {
-        if (num2 != 0) {
-            result = num1 / num2;
-        } else {
-            result = "Error: division by Zero";
-        }
-
-    } else if (operator === "*") {
-        result = num1 * num2;
-    } else if (operator === "%") {
-        if (num2 != 0) {
-            result = num1 % num2;
-        } else {
-            result = "Error: modulo by Zero";
-        }
-    } else {
-        result = "Error";
     }
-
+    else if (operator == "-") {
+        result = num1 - num2;
+    }
+    else if (operator == "*") {
+        result = num1 * num2;
+    }
+    else if (operator == "/") {
+        if (num2 == 0) {
+            display.value = "Error";
+            return;
+        }
+        result = num1 / num2;
+    }
+    else if (operator == "%") {
+        result = num1 % num2;
+    }
     document.getElementById("calculation").value = result;
+    display.value = result;
 }
-
 function clearAll() {
-    currentInput = "";
-    display.value = "0";
+    display.value = "";
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
 }
 
 function del() {
-    currentInput = currentInput.slice(0, -1);
-    display.value = currentInput || "0";
+    display.value = display.value.slice(0, -1);
 }
